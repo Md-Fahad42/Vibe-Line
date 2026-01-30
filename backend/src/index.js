b5b1bc5fe559abebeb62fccaeed2a40487f9a123
Md-Fahad42 <fahadansari2227@gmail.com>
2026-01-31 02:43:47 +0530

    resolving cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://vibe-line-orcin.vercel.app/",
     credentials: true,
   })
 );
